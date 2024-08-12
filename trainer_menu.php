bb0e27ca0008c7caf0a7aa0043cf697094ffc9e4
Waleed-Asaad <wga.nad@gmail.com>
2024-08-12 10:27:38 +0300

    aug 12 waleed C

--- trainer_menu.php
+++ trainer_menu.php
@@ -14,12 +14,7 @@
         <nav class="canvas-menu mobile-menu">
             <ul>
                 <li><a href="./trainer_home.php">Trainer Home</a></li>
-                <li><a href="#">Store</a>
-                    <ul class="dropdown">
-                        <li><a href="./store.php">Store</a></li>
-                        <li><a href="./history.php">History</a></li>
-                    </ul>
-                </li>
+                <li><a href="./store.php">Store</a></li>
                 <li><a href="./cart.php">Cart <i class="fas fa-shopping-cart"></i></a></li>
                 <li><a href="./trainerSchedule.php">My Schedule</a></li>
                 <li><a href="./myTrainees.php">My Trainees</a></li>
--- userMenu.php
+++ userMenu.php
@@ -47,12 +47,7 @@
                     <nav class="nav-menu">
                         <ul>
                             <li><a href="./user-home.php">User Home</a></li>
-                            <li><a href="#">Store</a>
-                                <ul class="dropdown">
-                                    <li><a href="./store.php">Store</a></li>
-                                    <li><a href="./history.php">History</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="./store.php">Store</a></li>
                             <li><a href="./subscription.php">Subscription</a></li>
                             <li><a href="./cart.php">Cart <i class="fas fa-shopping-cart"></i></a></li>
                             <li><a href="./logout.php">Logout <i class="fas fa-sign-out-alt"></i></a></li>
