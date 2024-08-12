c0e3ea243e5edb85547fac27e45de875f19cf007
amirAndessam <eesaamm@gmail.com>
2024-08-12 14:36:08 +0300

    august12essam1

--- trainer_home.php
+++ trainer_home.php
@@ -1,10 +1,43 @@
 <?php 
 include "connection.php";
 session_start();
+if (isset($_POST['submit'])) {
+    $target = "img/team/" . basename($_FILES['image']['name']);
+    $name = $_POST['name'];
+    $muscle_building = isset($_POST['muscle_building']) ? 1 : 0;
+    $weight_loss = isset($_POST['weight_loss']) ? 1 : 0;
+    $strength = isset($_POST['strength']) ? 1 : 0;
+    $endurance = isset($_POST['endurance']) ? 1 : 0;
+    $flexibility = isset($_POST['flexibility']) ? 1 : 0;
+    $body_building = isset($_POST['body_building']) ? 1 : 0;
+    $user_email = $_SESSION['userEmail'];
+    $select = "SELECT * FROM user WHERE userEmail = '$user_email'";
+    $result = mysqli_query($conn, $select);
+    $row = mysqli_fetch_array($result);
+    $user_id = $row['userId'];
+    $image = $_FILES['image']['name'];
+
+    $sql = "UPDATE trainer SET trainerImg = ?, trainerName = ?, muscle_building = ?, weight_loss = ?, strength = ?, flexibility = ?, endurance = ?, body_building = ? WHERE userId = ?";
+    $stmt = $conn->prepare($sql);
+    if ($stmt) {
+        $stmt->bind_param("ssiiiiiii", $image, $name, $muscle_building, $weight_loss, $strength, $flexibility, $endurance, $body_building, $user_id);
+        if ($stmt->execute()) {
+            move_uploaded_file($_FILES['image']['tmp_name'], $target);
+            echo "Record updated successfully";
+        } else {
+            echo "Error updating record: " . $stmt->error;
+        }
+        $stmt->close();
+    } else {
+        echo "Error preparing statement: " . $conn->error;
+    }
+
+    header("Location: trainer_home.php");
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="zxx">
-
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Gym Template">
@@ -26,35 +59,208 @@ session_start();
     <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <style>
+        .star-rating1 {
+            position: relative;
+            display: inline-flex;
+            flex-direction: row-reverse;
+            justify-content: flex-end;
+            margin: 0 -0.25rem;
+        }
+
+        .star-input {
+            position: absolute;
+            opacity: 0;
+        }
+
+        .star-label {
+            cursor: pointer;
+            color: grey;
+            padding: 0 0.25rem;
+            transition: color 0.15s;
+        }
+
+        .star-input:checked ~ label {
+            color: gold;
+        }
+
+        .star-input:hover ~ label {
+            color: goldenrod;
+            transition: none;
+        }
+
+        .star-label:active {
+            color: darkgoldenrod !important;
+        }
+
+        .star-input:focus-visible + label {
+            outline-offset: 1px;
+            outline: #4f46e5 solid 2px;
+        }
+
+        .star-rating {
+            display: flex;
+            flex-direction: row-reverse;
+            font-size: 1.5rem;
+            justify-content: center;
+            padding: 0.3rem;
+        }
+        .star-rating span {
+            cursor: default;
+            color: grey;
+            padding: 0 0.1rem;
+        }
+        .star-rating .checked {
+            color: gold;
+        }
+        
+    </style>
 </head>
 
 <body>
-<?php
-    include 'trainer_Menu.php';
-?>
+<?php include 'Trainer_menu.php'; ?>
 
     <!-- Hero Section Begin -->
     <section class="hero-section">
         <div class="hs-slider owl-carousel">
-            <div class="hs-item set-bg" data-setbg="img/hero/hero-1.jpg">
+            <div style="height:1500px" class="hs-item set-bg" data-setbg="img/hero/hero-1.jpg">
+
+            <?php
+                                $user_email = $_SESSION['userEmail'];
+                                $select = "SELECT * FROM user WHERE userEmail = '$user_email'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                $user_id = $row['userId'];
+
+                                $select = "SELECT * FROM trainer WHERE userId = '$user_id'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                
+                                $traineeImg = $row['trainerImg'];
+                                echo '
+                                <div style="width:400px;height:500px; margin-left:100px; margin-top:80px ;margin-right:auto;" class="gs-item grid-wide set-bg" data-setbg="img/team/'.$traineeImg.'"></div>
+                                ';
+                ?>
                 <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 offset-lg-6">
-                            <div class="hi-text">
-                                <span>Shape your body</span>
-                                <h1>Be <strong>strong</strong> traning hard</h1>
-                            </div>
+                    <div class="col-lg-10 col-md-8">
+                        <div class="ps-item">
+                            <h3 style="font-size:40px">Personal Details</h3>
+                            <?php
+                                $user_email = $_SESSION['userEmail'];
+                                $select = "SELECT * FROM user WHERE userEmail = '$user_email'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                $user_id = $row['userId'];
+
+                                $select = "SELECT * FROM trainer WHERE userId = '$user_id'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                $name = $row['trainerName'];
+                                $muscle_building = $row['muscle_building'];
+                                $weight_loss = $row['weight_loss'];
+                                $strength = $row['strength'];
+                                $endurance = $row['endurance'];
+                                $body_building = $row['body_building'];
+                                $flexibility = $row['flexibility'];
+                                $cancel = $row['cancel'];
+                                $rating = $row['rating'];
+                                $trainerImg = $row['trainerImg'];
+                                echo '
+                                <ul>
+                                    
+                                    <div class="specialty">
+                                    <li style="font-size:25px;margin-bottom: 20px">Name: ' . htmlspecialchars($name) . '</li>
+                                    <li style="font-size:25px;margin-bottom: 5px">Speciality:</li>';
+                                    if ($muscle_building) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Muscle Building</li>';
+                                    }
+                                    if ($weight_loss) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Weight Loss</li>';
+                                        
+                                    }
+                                    if ($strength) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Strength</li>';
+                                        
+                                    }
+                                    if ($endurance) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Endurance</li>';
+                                        
+                                    }
+                                    if ($body_building) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Bodybuilding</li>';
+                                        
+                                    }
+                                    if ($flexibility) {
+                                        echo '<li style="font-size:25px;margin-bottom: 5px">Flexibility</li>';
+                                        
+                                    }
+                                    echo '</div>
+                                    <li style="font-size:25px;margin-bottom: 20px">Cancels: ' . htmlspecialchars($cancel) . '</li>
+                                    <div  class="star-rating">';
+                                    for ($i = 5; $i >= 1; $i--) {
+                                        $checked = ($i <= round($rating)) ? "checked" : "";
+                                        echo '<span  class="fa fa-star '.$checked.'"></span>';
+                                    }
+                                     echo '  </div>
+                                    <li style="font-size:35px;margin-bottom: 5px;color:orange"><b>Slide To The Side To Update</b></li>
+                                </ul>';
+                            ?>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="hs-item set-bg" data-setbg="img/hero/hero-2.jpg">
+            <div style="height:1500px" class="hs-item set-bg" data-setbg="img/hero/hero-2.jpg">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-6 offset-lg-6">
+                        <div class="col-lg-6 offset-lg-4">
                             <div class="hi-text">
-                                <span>Shape your body</span>
-                                <h1>Be <strong>strong</strong> traning hard</h1>
+                                <div class="container" style="width:800px;">
+                                    <div class="form-container" style="padding:5px 5px 5px 5px">
+                                        <form action="" method="post" enctype="multipart/form-data" style="width:800px;">
+                                            <h1 style="font-size:35px;margin-bottom: 0;">Update Personal details</h1>
+                                            <?php if (isset($err)) {
+                                                foreach ($err as $err) {
+                                                    echo '<span class="error-msg">' . htmlspecialchars($err) . '</span>';
+                                                }
+                                            } ?>
+                                            <?php
+                                $user_email = $_SESSION['userEmail'];
+                                $select = "SELECT * FROM user WHERE userEmail = '$user_email'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                $user_id = $row['userId'];
+
+                                $select = "SELECT * FROM trainee WHERE userId = '$user_id'";
+                                $result = mysqli_query($conn, $select);
+                                $row = mysqli_fetch_array($result);
+                                
+                                $name = $row['trainerName'];
+                                $name_placeholder = isset($height) ? $height : 'Enter your name';
+                                echo '
+                                            <input type="text" name="name" required placeholder="'.$name_placeholder.'">
+                                            <div class="specialty">
+                                                <label style="color:white">Specialty:</label><br>
+                                                <label for="muscle_building" style="color:white">Muscle Building</label><br>
+                                                <input type="checkbox" id="muscle_building" name="muscle_building" value="1"><br>
+                                                <label for="weight_loss" style="color:white">Weight Loss</label><br>
+                                                <input type="checkbox" id="weight_loss" name="weight_loss" value="1"><br>
+                                                <label for="strength" style="color:white">Strength</label><br>
+                                                <input type="checkbox" id="strength" name="strength" value="1"><br>
+                                                <label for="endurance" style="color:white">Endurance</label><br>
+                                                <input type="checkbox" id="endurance" name="endurance" value="1"><br>
+                                                <label for="flexibility" style="color:white">Flexibility</label><br>
+                                                <input type="checkbox" id="flexibility" name="flexibility" value="1"><br>
+                                                <label for="body_building" style="color:white">Body Building</label><br>
+                                                <input type="checkbox" id="body_building" name="body_building" value="1"><br>
+                                            </div>
+                                            <input type="file" name="image" accept="image/png, image/jpg, image/jpeg" required>
+                                            <input type="submit" name="submit" value="Submit" class="form-btn">
+                                        ';
+                                        ?>
+                                            </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -80,8 +286,7 @@ session_start();
                     <div class="cs-item">
                         <span class="flaticon-034-stationary-bike"></span>
                         <h4>Modern equipment</h4>
-                        <p>At our gym, we offer the newest and best fitness equipment on the market,
-                             ensuring our members get the highest quality workout experience.</p>
+                        <p>At our gym, we offer the newest and best fitness equipment on the market, ensuring our members get the highest quality workout experience.</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-sm-6">
@@ -94,7 +299,7 @@ session_start();
                 <div class="col-lg-3 col-sm-6">
                     <div class="cs-item">
                         <span class="flaticon-002-dumbell"></span>
-                        <h4>professional training plan</h4>
+                        <h4>Professional training plan</h4>
                         <p>Our professional training plan is tailored to guide you to peak performance and optimal results.</p>
                     </div>
                 </div>
@@ -110,67 +315,13 @@ session_start();
     </section>
     <!-- ChoseUs Section End -->
 
-    <!-- Banner Section Begin -->
-    <!-- <section class="banner-section set-bg" data-setbg="img/banner-bg.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="bs-text">
-                        <h2>registration now to get more deals</h2>
-                        <div class="bt-tips">Where health, beauty and fitness meet.</div>
-                        <a href="#" class="primary-btn  btn-normal">subscribe</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- Banner Section End -->
-
-    <!-- Pricing Section Begin -->
-    <!-- <?php
-        include 'pricing.php';
-    ?> -->
-    <!-- Pricing Section End -->
-
-    <!-- Gallery Section Begin -->
-    <!-- <div class="gallery-section">
-        <div class="gallery">
-            <div class="grid-sizer"></div>
-            <div class="gs-item grid-wide set-bg" data-setbg="img/gallery/gallery-1.jpg">
-                <a href="img/gallery/gallery-1.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-            <div class="gs-item set-bg" data-setbg="img/gallery/gallery-2.jpg">
-                <a href="img/gallery/gallery-2.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-            <div class="gs-item set-bg" data-setbg="img/gallery/gallery-3.jpg">
-                <a href="img/gallery/gallery-3.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-            <div class="gs-item set-bg" data-setbg="img/gallery/gallery-4.jpg">
-                <a href="img/gallery/gallery-4.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-            <div class="gs-item set-bg" data-setbg="img/gallery/gallery-5.jpg">
-                <a href="img/gallery/gallery-5.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-            <div class="gs-item grid-wide set-bg" data-setbg="img/gallery/gallery-6.jpg">
-                <a href="img/gallery/gallery-6.jpg" class="thumb-icon image-popup"><i class="fa fa-picture-o"></i></a>
-            </div>
-        </div>
-    </div> -->
-    <!-- Gallery Section End -->
-
     <!-- Get In Touch Section Begin -->
-    <?php 
-        include 'getInTouch.php';
-    ?>
+    <?php include 'getInTouch.php'; ?>
     <!-- Get In Touch Section End -->
 
     <!-- Footer Section Begin -->
-    <?php 
-        include 'footer.php';
-    ?>
+    <?php include 'footer.php'; ?>
     <!-- Footer Section End -->
-
-    
 
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
@@ -181,9 +332,5 @@ session_start();
     <script src="js/jquery.slicknav.js"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/main.js"></script>
-
-
-
 </body>
-
 </html>
